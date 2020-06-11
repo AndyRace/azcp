36fcd9e6fe8bf50ef79b990997089a12ec60914d
Andy Race <Andy.Race@willistowerswatson.com>
2020-06-11 03:11:22 +0100

    Added debugging info around the git tagging

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,9 @@
 ﻿# e.g. v0.1-0-g6861947-dirty
-# git describe --long --tags --dirty --always
-# git rev-list HEAD | wc -l
-# git rev-parse HEAD
+
+git describe --long --tags --dirty --always
+git rev-list HEAD | wc -l
+git rev-parse HEAD
+
 $gitVersion=git describe --long --tags --dirty --always --match 'v[0-9]*\.[0-9]*'
 
 if ($gitVersion -match 'v(\d+\.\d+)-(\d+)-(.*)') {
@@ -13,4 +15,6 @@ if ($gitVersion -match 'v(\d+\.\d+)-(\d+)-(.*)') {
 }
 
 #dotnet restore
+
+Write-Host "Publishing $informationalVersion ($fileVersion)"
 dotnet publish /p:DeployOnBuild=true /p:PublishProfile=FolderProfile /p:FileVersion=$fileVersion /p:InformationalVersion=$informationalVersion
